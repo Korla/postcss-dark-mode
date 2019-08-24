f648965641c1fe7f10b5316686ffd2939f19cd01
korla <bengtsson.johannes@gmail.com>
2019-08-25 01:29:52 +0200

    Simplify loops, only looping regexes once

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-let postcss = require('postcss')
+const postcss = require('postcss')
 
+// The order of the regexes matters since shorter regexes can also match the longer ones. The shorter 
+// will replace the longer ones before they are run if they are run first.
 const regexes = [
-  /#([A-Fa-f0-9]{6})([; ,]|)/gi,
-  /#([A-Fa-f0-9]{3})([; ,]|)/gi
+  /#([A-Fa-f0-9]{6})([; ,]|)/gi, // #ffeeff
+  /#([A-Fa-f0-9]{3})([; ,]|)/gi // #ffe
 ]
 
 module.exports = postcss.plugin(
   'postcss-dark-mode',
   opts => (root, result) => {
     root.walkRules(rule => {
+      // Walk all rules in the stylesheet, if rule contains no declarations of color, the rule should be removed.
       let ruleHasMatch = false;
       rule.walkDecls(decl => {
-        const allMatches = [];
-        regexes.forEach(r => allMatches.push.apply(allMatches, (decl.value.match(r) || []).map(m => ({ match: m, regex: r }))))
-        if (!allMatches.length) {
+        // Walk all declarations in a rule, if the declaration contains no color, the declaration is removed.
+        let declHasMatch = false;
+        regexes.forEach(regex => {
+          (decl.value.match(regex) || []).forEach(match => {
+            // If a match is found, the rule and declaration is kept.
+            declHasMatch = true;
+            ruleHasMatch = true;
+            // TODO: If no matching color is found, a warning is created.
+            // Each match which is found is run again, this time replacing the found value with the mapped value.
+            decl.value = decl.value.replace(regex, opts[match])
+          })
+        })
+        if (!declHasMatch) {
           decl.remove()
-        } else {
-          allMatches.forEach(m => decl.value = decl.value.replace(m.regex, opts[m.match]))
-          ruleHasMatch = true;
         }
       })
       if (!ruleHasMatch) {
--- index.test.js
+++ index.test.js
@@ -2,13 +2,13 @@ var postcss = require('postcss')
 var traderacss = require('./traderacss')
 var plugin = require('./')
 
-const run = (input, output, opts) => () => postcss([plugin(opts)]).process(input).then(result => {
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
-})
+const run = (input, opts, resultCallback) => () => postcss([plugin(opts)]).process(input).then(resultCallback)
 
 it('Maps colors from the color map, removing declarations and rules lacking colors', run(
-  'a { color: #ffe; border-radius: 5px; background-color: #ffeffe; } a:hover { text-decoration: underline; }',
-  'a { color: dark-white; background-color: another-white; }',
-  { '#ffe': 'dark-white', '#ffeffe': 'another-white' }
+  `a { color: #ffe; border-radius: 5px; background-color: #ffeeff; } a:hover { text-decoration: underline; }`,
+  { '#ffe': 'dark-white', '#ffeeff': 'another-white' },
+  result => {
+    expect(result.css).toEqual('a { color: dark-white; background-color: another-white; }')
+    expect(result.warnings()).toHaveLength(0)
+  }
 ))
