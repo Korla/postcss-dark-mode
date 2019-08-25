3cf99263f88adc28303a273f362182efd023fc52
korla <bengtsson.johannes@gmail.com>
2019-08-25 10:13:54 +0200

    Separate tests for test cases

--- index.test.js
+++ index.test.js
@@ -4,11 +4,29 @@ var plugin = require('./')
 
 const run = (input, opts, resultCallback) => () => postcss([plugin(opts)]).process(input).then(resultCallback)
 
-it('Maps colors from the color map, removing declarations and rules lacking colors', run(
-  `a { color: #ffe; border-radius: 5px; background-color: #ffeeff; } a:hover { text-decoration: underline; }`,
-  { '#ffe': 'dark-white', '#ffeeff': 'another-white' },
+it('Maps colors using the color map', run(
+  `a { color: #ffe; border-radius: 5px; background-color: #ffeeff; }`,
+  { '#ffe': 'red', '#ffeeff': 'blue' },
   result => {
-    expect(result.css).toEqual('a { color: dark-white; background-color: another-white; }')
+    expect(result.css).toEqual('a { color: red; background-color: blue; }')
+    expect(result.warnings()).toHaveLength(0)
+  }
+))
+
+it('Removes rules which lack color declaraions', run(
+  `a { color: #ffe; } a:hover { text-decoration: underline; }`,
+  { '#ffe': 'red' },
+  result => {
+    expect(result.css).toEqual('a { color: red; }')
+    expect(result.warnings()).toHaveLength(0)
+  }
+))
+
+it('Removes declarations which lack color', run(
+  `a { color: #ffe; border-radius: 5px; }`,
+  { '#ffe': 'red' },
+  result => {
+    expect(result.css).toEqual('a { color: red; }')
     expect(result.warnings()).toHaveLength(0)
   }
 ))
