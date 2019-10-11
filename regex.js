1586026eca21ee9a35283b7097ebbba716193eb4
Johannes Bengtsson <bengtsson.johannes@gmail.com>
2019-10-11 15:35:31 +0200

    hsl, and hsla regex and tests

--- regex.js
+++ regex.js
@@ -4,5 +4,7 @@ module.exports = {
   shortHexa: /#([A-Fa-f0-9]{3})(?=($|\s))/gi,
   longHexa: /#([A-Fa-f0-9]{6})(?=($|\s))/gi,
   rgb: /rgb\((\d+),(\d+),(\d+)\)(?=($|\s))/gi,
-  rgba: /rgba\((\d+),(\d+),(\d+)\)(?=($|\s))/gi
+  rgba: /rgba\((\d+),(\d+),(\d+)\)(?=($|\s))/gi,
+  hsl: /hsl\((\d+),(\d+),(\d+)\)(?=($|\s))/gi,
+  hsla: /hsla\((\d+),(\d+),(\d+)\)(?=($|\s))/gi
 };
--- regex.test.js
+++ regex.test.js
@@ -1,4 +1,4 @@
-const { shortHexa, longHexa, rgb, rgba } = require('./regex');
+const { shortHexa, longHexa, rgb, rgba, hsl, hsla } = require('./regex');
 
 it('Short hexa', () => {
   const regex = shortHexa;
@@ -30,4 +30,20 @@ it('rgba', () => {
   expect(' rgba(1,1,1)'.match(regex)).toEqual(['rgba(1,1,1)']);
   expect('rgba(1,1,1);'.match(regex)).toEqual(null);
   expect('rgb(1,1,1);'.match(regex)).toEqual(null);
+});
+
+it('hsl', () => {
+  const regex = hsl;
+  expect('hsl(1,1,1)'.match(regex)).toEqual(['hsl(1,1,1)']);
+  expect(' hsl(1,1,1)'.match(regex)).toEqual(['hsl(1,1,1)']);
+  expect('hsl(1,1,1);'.match(regex)).toEqual(null);
+  expect('hsla(1,1,1);'.match(regex)).toEqual(null);
+});
+
+it('hsla', () => {
+  const regex = hsla;
+  expect('hsla(1,1,1)'.match(regex)).toEqual(['hsla(1,1,1)']);
+  expect(' hsla(1,1,1)'.match(regex)).toEqual(['hsla(1,1,1)']);
+  expect('hsla(1,1,1);'.match(regex)).toEqual(null);
+  expect('hsl(1,1,1);'.match(regex)).toEqual(null);
 });
